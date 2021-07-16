c10ccd1ed705aa3da28e602a1258d51463e40755
Hideo Terada <h.tee2006@gmail.com>
2021-07-16 17:33:56 +0900

    EDIT

--- build.sh
+++ build.sh
@@ -1,31 +1,24 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
-
-cd Thirdparty/DBoW2
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-
-cd ../../g2o
-
 echo "Configuring and building Thirdparty/g2o ..."
 
+pushd Thirdparty/g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
+popd
 
-cd ../../../
 
 echo "Uncompress vocabulary ..."
 
-cd Vocabulary
+pushd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
-cd ..
+popd
 
 echo "Configuring and building ORB_SLAM2 ..."
 
-mkdir build
+pushd build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
+popd
+
